58330cde64d3f06fc61098f7f2c0cf1528b93a53
lexx24061971 <lexx2406@yandex.ru>
2023-05-06 00:55:48 +0400

    Пересборка

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -30,24 +30,122 @@ final class MovieQuizViewController: UIViewController {
         
     }
     @IBOutlet weak private var labelQuestion: UILabel!
-    
+    struct ViewModel {
+      let image: UIImage
+      let question: String
+      let questionNumber: String
+    }
+    struct QuizQuestion {
+        let image: String
+        let text: String
+        let correctAnswer: Bool
+    }
+    struct QuizStepViewModel {
+        let image: UIImage
+        let question: String
+        let questionNumber: String
+    }
+    struct QuizResultsViewModel {
+      let title: String
+      let text: String
+      let buttonText: String
+    }
+    private let questions: [QuizQuestion] = [
+        QuizQuestion(
+            image: "The Godfather",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Dark Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Kill Bill",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Avengers",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Deadpool",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Green Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Old",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "The Ice Age Adventures of Buck Wild",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Tesla",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Vivarium",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false)
+    ]
+    private var currentQuestionIndex = 0
+    private var correctAnswers = 0
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        let questionStep = QuizStepViewModel(
+            image: UIImage(named: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+        return questionStep
+    }
     private func show(quiz step: QuizStepViewModel) {
         imageView.image = step.image
         textLabel.text = step.question
         counterLabel.text = step.questionNumber
     }
-    
     private func showNextQuestionOrResults() {
-        if currentQuestionIndex == questions.count - 1 { // 1
-            // идём в состояние "Результат квиза"
+        if currentQuestionIndex == questions.count - 1 {
+            let text = "Ваш результат: \(correctAnswers)/10"
+            let viewModel = QuizResultsViewModel(
+                title: "Этот раунд окончен!",
+                text: text,
+                buttonText: "Сыграть ещё раз")
+            show(quiz: viewModel)
         } else {
             currentQuestionIndex += 1
             let nextQuestion = questions[currentQuestionIndex]
-            let newModel = convert(model: nextQuestion)
+            let viewModel = convert(model: nextQuestion)
+            
             show(quiz: viewModel)
         }
+   
+    }
+    private func show(quiz result: QuizResultsViewModel) {
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.text,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+            self.currentQuestionIndex = 0
+            self.correctAnswers = 0
+            
+            let firstQuestion = self.questions[self.currentQuestionIndex]
+            let viewModel = self.convert(model: firstQuestion)
+            self.show(quiz: viewModel)
+        }
+        
+        alert.addAction(action)
+        
+        self.present(alert, animated: true, completion: nil)
     }
     private func showAnswerResult(isCorrect: Bool) {
+        if isCorrect {
+            correctAnswers += 1
+        }
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
         imageView.layer.borderColor = isCorrect ? UIColor.ysGreen.cgColor : UIColor.ysRed.cgColor
@@ -55,81 +153,11 @@ final class MovieQuizViewController: UIViewController {
             self.showNextQuestionOrResults()
         }
     }
-    
-    
-    let alert = UIAlertController(
-        title: "Этот раунд окончен!",
-        message: "Ваш результат ???",
-        preferredStyle: .alert)
-    
-    
-    private let questions: [QuizQuestion] = [
-        QuizQuestion (image: "The Godfather",
-                      question: "Рейтинг этого фильма больше чем 6?",
-                      correctAnswer: true),
-        QuizQuestion (image: "The Dark Knight",
-                      question: "Рейтинг этого фильма больше чем 6?",
-                      correctAnswer: true),
-        
-        QuizQuestion (image: "Kill Bill",
-                      question: "Рейтинг этого фильма больше чем 6?",
-                      correctAnswer: true),
-        QuizQuestion (image: "The Avengers",
-                      question: "Рейтинг этого фильма больше чем 6?",
-                      correctAnswer: true),
-        QuizQuestion (image: "Deadpool",
-                      question: "Рейтинг этого фильма больше чем 6?",
-                      correctAnswer: true),
-        QuizQuestion (image: "The Green Knight",
-                      question: "Рейтинг этого фильма больше чем 6?",
-                      correctAnswer: true),
-        
-        QuizQuestion (image: "Old",
-                      question: "Рейтинг этого фильма больше чем 6?",
-                      correctAnswer: false),
-        QuizQuestion (image: "The Ice Age Adventures of Buck Wild",
-                      question: "Рейтинг этого фильма больше чем 6?",
-                      correctAnswer: false),
-        QuizQuestion (image: "Tesla",
-                      question: "Рейтинг этого фильма больше чем 6?",
-                      correctAnswer: false),
-        
-        QuizQuestion (image: "Vivarium",
-                      question: "Рейтинг этого фильма больше чем 6?",
-                      correctAnswer: false),
-    ]
-    
-    struct QuizQuestion {
-        let image:String
-        let question:String
-        let correctAnswer:Bool
-    }
-    private var currentQuestionIndex = 0
-    private var correctAnswer = 0
-    struct QuizStepViewModel {
-        let image: UIImage
-        let question: String
-        let questionNumber: String
-    }
-    private func convert(model: QuizQuestion) -> QuizStepViewModel {
-        let questionStep = QuizStepViewModel(
-            image: UIImage(named: model.image) ?? UIImage(),
-            question: model.question,
-            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
-        return questionStep
-    }
-    let alert = UIAlertController(
-        title: "Этот раунд окончен!",
-        message: "Ваш результат ???",
-        preferredStyle: .alert) {
-            let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
-                self.currentQuestionIndex = 0
-                self.correctAnswers = 0
-                let firstQuestion = self.questions[self.currentQuestionIndex]
-                let viewModel = self.convert(model: firstQuestion)
-                self.show(quiz: viewModel)
-            }
-            alert.addAction(action)
-            self.present(alert, animated: true, completion: nil)
-        }
 }
+            
+        
+    
+    
+    
+    
+    
